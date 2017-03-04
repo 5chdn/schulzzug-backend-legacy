39814fb7841883d6680ddb17e2419a107448572b
René Veenhuis <re.veenhuis@gmail.com>
2017-03-04 20:36:50 +0100

    Clean up

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var	express = require('express'),
 	randtoken = require('rand-token'),
 	MongoClient = require('mongodb').MongoClient;
 
+var mongodbHost = process.env.database || "localhost";
+
 var app = express();
+MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, connection) {
+	app.database = connection;
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,10 +26,7 @@ app.post('/register', function(req, res) {
 		user: user,
 		token: randtoken.generate(16)
 	}
-
-	var mongodbHost = process.env.database || "localhost";
-	MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, connection) {
-		let collection = connection.collection('users');
+		let collection = app.database.collection('users');
 		let insertData = {
 			username: answer.user,
 			token: answer.token
@@ -33,7 +35,6 @@ app.post('/register', function(req, res) {
 		collection.insert(insertData, {safe: true}, function(err, data) {
     		console.log("done!");
     	});
-	});	
 
 	res.send(answer);
 });
@@ -41,53 +42,45 @@ app.post('/register', function(req, res) {
 app.put('/score/:user', function(req, res) {
 	let user = req.params.user,
 		score = req.body.score;
-	
-	var mongodbHost = process.env.database || "localhost";
-	MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, connection) {
-		let collection = connection.collection('score');
-		let insertData = {
-			username: user,
-			score: score
-		};
 
-		collection.insert(insertData, {safe: true}, function(err, data) {
-    		console.log("done!");
-    	});
-	});
+	let insertData = {
+		username: user,
+		score: score
+	};
+
+	let collection = app.database.collection('score');
+	collection.insert(insertData, {safe: true}, function(err, data) {
+    	console.log("done!");
+    });
 
 	res.send(user);
 });
 
 app.get('/score', function(req, res) {
-	var mongodbHost = process.env.database || "localhost";
-	MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, connection) {
-		let collection = connection.collection('scores');
-			let options = {
-				"sort": [["score", "desc"]],
-				"limit": 1
-			};
-			collection.findOne({}, options, function(err, answer) {
-			res.send(answer);
-		});
+	let collection = app.database.collection('scores');
+	let options = {
+		"sort": [["score", "desc"]],
+		"limit": 1
+	};
+
+	collection.findOne({}, options, function(err, answer) {
+		res.send(answer);
 	});
 });
 
 app.get('/score/:user', function(req, res) {
 	var user = req.params.user;
 
-	var mongodbHost = process.env.database || "localhost";
-	MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, connection) {
-		let collection = connection.collection('scores');
-			let options = {
-				"sort": [["score", "desc"]],
-				"limit": 1
-			};
-			collection.findOne({"username": user}, options, function(err, answer) {
-			res.send(answer);
-		});
+	let collection = app.database.collection('scores');
+	let options = {
+		"sort": [["score", "desc"]],
+		"limit": 1
+	};
+	collection.findOne({"username": user}, options, function(err, answer) {
+		res.send(answer);
 	});
 });
 
 app.listen(8080, function(){
-	console.log('listening on *:8080');
+	console.log('listening on *:3000');
 });
