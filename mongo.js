f74ee42b2a6a9eaf08c16eca7a5158193c9b7776
René Veenhuis <re.veenhuis@gmail.com>
2017-03-04 19:50:12 +0100

    Add mongo-db migration

---
+++ mongo.js
@@ -0,0 +1,46 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+
+var testUser = {
+	_id: 0,
+	username: 'peter',
+	token: 'EstnUFIXM1Z34tAC'
+};
+
+var testScore = {
+	_id: 0,
+	username: 'peter',
+	score: 123
+};
+
+var mongodbHost = process.env.database || "localhost";
+MongoClient.connect("mongodb://" + mongodbHost + ":27017/spdhack", function(err, db) {
+	console.log("connected");
+
+	var userCollection = db.collection('users');
+	var scoreCollection = db.collection('scores');
+
+	var counter = 0;
+	function updateCounter() {
+		counter++;
+		if(counter == 2) {
+			console.log("Done!");
+			process.exit();
+		}
+	}
+
+	userCollection.insert(testUser, {safe: true}, function(err, data) {
+		userCollection.find({_id: 0}).toArray(function(err, data) {
+			console.log(data);
+			updateCounter();
+		});
+	});
+
+	scoreCollection.insert(testScore, {safe: true}, function(err, data) {
+		scoreCollection.find({_id: 0}).toArray(function(err, data) {
+			console.log(data);
+			updateCounter();
+		});
+	});
+});
