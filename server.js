678319c60467ea9770041498899a2c3fafdbcef2
René Veenhuis <re.veenhuis@gmail.com>
2017-03-04 15:57:29 +0100

    Implement register function

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+"use strict";
+
 var	express = require('express'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	randtoken = require('rand-token');
 
 var app = express();
 
 app.use(bodyParser.json());
+
+app.get('/register', function(req, res) {
+	let answer = {
+		token: randtoken.generate(16)
+	}
+	res.send(answer);
+});
 
 app.listen(8080, function(){
 	console.log('listening on *:3000');
